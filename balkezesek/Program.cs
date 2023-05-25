1a6e4740ad0c756513b50de2a83a4b4cc8b70a36
bekesi142 <bekesibalint2006@gmail.com>
2023-05-25 12:51:47 +0200

    ebéd után

--- balkezesek/Program.cs
+++ balkezesek/Program.cs
@@ -6,6 +6,8 @@ StreamReader sr = new StreamReader("balkezesek.csv");
 
 
 List<string> nev_lista = new List<string>();
+List<string> elso_lista = new List<string>();
+List<string> utolso_lista = new List<string>();
 List<int> suly_lista = new List<int>();
 List<int> magassag_lista = new List<int>();
 
@@ -17,12 +19,39 @@ while (!sr.EndOfStream)
     string sor = sr.ReadLine();
     hanysorvan++;
     string nev = sor.Split(";")[0];
+    string elso = sor.Split(";")[1];
+    string utolso = sor.Split(";")[2];
     int suly = int.Parse(sor.Split(";")[3]);
     int magassag = int.Parse(sor.Split(";")[4]);
     nev_lista.Add(nev);
+    elso_lista.Add(elso);
+    utolso_lista.Add(utolso);
     magassag_lista.Add(magassag);
     suly_lista.Add(suly);
     
 }
+List<string> szukitett_lista = new List<string>();
+List<int> jodatum_index_lista = new List<int>();
+List<string> jonev_index_lista = new List<string>();
+
+foreach (string datum in utolso_lista)
+{
+    if (datum.Contains("1999-10"))
+    {
+        szukitett_lista.Add(datum);
+        jodatum_index_lista.Add(utolso_lista.IndexOf(datum));
+    }
+}
+
+foreach (int index in jodatum_index_lista)
+{
+    foreach (string nev in nev_lista)
+    {
+        jonev_index_lista.Add(nev_lista[index]);
+
+    }
+}
+
 
 Console.WriteLine("3. feladat:  " + hanysorvan);
+Console.WriteLine("4. feladat:  " + szukitett_lista);
